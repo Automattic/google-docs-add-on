da5319b10215126afb9fd8cb96a9af5e65a1dc4b
George Hotelling <george.hotelling@automattic.com>
2017-01-05 16:53:36 -0500

    Successful test posted to multiple sites

--- src/sites.js
+++ src/sites.js
@@ -2,24 +2,44 @@ const PERSISTANCE_KEY = 'SITES'
 
 export function Sites( propertieService ) {
 	const props = () => propertieService.getUserProperties();
-
-	function add( site ) {
-		const sites = list()
-		props().setProperty( PERSISTANCE_KEY, JSON.stringify( sites.concat( site ) ) )
-	}
+	let sitesCache;
 
 	function list() {
-		let sites;
+		if ( sitesCache ) {
+			return sitesCache;
+		}
+
 		const jsonSites = props().getProperty( PERSISTANCE_KEY )
 		try {
-			sites = JSON.parse( jsonSites )
+			sitesCache = JSON.parse( jsonSites )
 		} catch ( e ) {
-			sites = []
+			sitesCache = []
 		}
-		return sites || [];
+		return sitesCache || [];
 	}
 
-	const find = site_id => list.find( site => site.site_id === site_id )
+	const find = site_id => {
+		const sites = list()
+		for ( let i = 0; i < sites.length; i++ ) {
+			if ( sites[ i ].blog_id === site_id ) {
+				return sites[ i ];
+			}
+		}
+	}
+
+	function add( site ) {
+		if ( find( site.blog_id ) ) {
+			return;
+		}
+		const sites = list()
+		sitesCache = sites.concat( siteIdentity( site ) );
+		props().setProperty( PERSISTANCE_KEY, JSON.stringify( sitesCache ) )
+	}
+
+	function siteIdentity( site ) {
+		const { access_token, blog_id, blog_url, info: { name } } = site;
+		return { access_token, blog_id, blog_url, info: { name } };
+	}
 
 	return {
 		add,
