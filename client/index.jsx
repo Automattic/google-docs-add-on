1ce4e2410a98c38f2b1bd3223915e2f3207bdbbc
George Hotelling <george.hotelling@automattic.com>
2017-03-21 16:55:59 -0400

    Finished moving everything into React
    Now I have to get it all working ¯\_(ツ)_/¯  (hope I remember to squash this commit message)

--- client/index.jsx
+++ client/index.jsx
@@ -6,7 +6,7 @@ class PostButton extends React.Component {
 		super( props );
 		this.state = {
 			disabled: props.disabled,
-			post: props.post
+			post: props.site.post
 		};
 		this.savePost = this.savePost.bind( this )
 	}
@@ -18,8 +18,8 @@ class PostButton extends React.Component {
 				this.setState( { disabled: null } )
 				this.setState( { post } )
 			} )
-			.catch( () => {
-				// TODO error handling
+			.catch( ( e ) => {
+				this.props.onError( e )
 				this.setState( { disabled: null } )
 			} )
 	}
@@ -31,41 +31,114 @@ class PostButton extends React.Component {
 	}
 }
 
-const Site = ( site ) => {
-	const blavatar = ( site.info.icon && site.info.icon.img ) ? site.info.icon.img : 'https://secure.gravatar.com/blavatar/e6392390e3bcfadff3671c5a5653d95b'
-	const previewLink = ( site.post ) ? <span className="sites-list__post-link"><a href={ site.post.URL }>Preview on { site.info.name }</a></span> : null;
-	const removeSite = () => deleteSite( site.blog_id ).then( reloadSites )
+const Site = ( props ) => {
+	const blavatar = ( props.site.info.icon && props.site.info.icon.img ) ? props.site.info.icon.img : 'https://secure.gravatar.com/blavatar/e6392390e3bcfadff3671c5a5653d95b'
+	const previewLink = ( props.site.post ) ? <span className="sites-list__post-link"><a href={ props.site.post.URL }>Preview on { props.site.info.name }</a></span> : null;
+	const removeSite = () => deleteSite( props.site.blog_id ).then( reloadSites ).catch( props.onError )
 
-
-	return <li key={ site.blog_id }>
+	return <li key={ props.site.blog_id }>
 		<div className="sites-list__blavatar">
 			<img src={ blavatar } alt="" />
 		</div>
 		<div className="sites-list__sitename">
-			<a className="sites-list__title" href={ site.blog_url }>{ site.info.name }<br />
-			<em>{ site.blog_url }</em></a>
+			<a className="sites-list__title" href={ props.site.blog_url }>{ props.site.info.name }<br />
+			<em>{ props.site.blog_url }</em></a>
 			<a title="Remove site from this list" className="sites-list__delete-site" onClick={ removeSite }><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect x="0" fill="none" width="24" height="24"/><g><path d="M17.705 7.705l-1.41-1.41L12 10.59 7.705 6.295l-1.41 1.41L10.59 12l-4.295 4.295 1.41 1.41L12 13.41l4.295 4.295 1.41-1.41L13.41 12l4.295-4.295z"/></g></svg></a>
 		</div>
-		<PostButton {...site} />
+		<PostButton {...props} />
 
 		{ previewLink }
 	</li>
 }
 
 const SiteList = ( props ) => {
-	if ( ! props.sites ) {
+	if ( 0 === props.sites.length ) {
 		return <h1>Loading…</h1>
 	}
 
 	return <ul>
-		{ props.sites.map( Site ) }
+		{ props.sites.map( site => <Site site={ site } onError={ props.onError } /> ) }
 	</ul>
 }
 
-function render( sites ) {
+const ErrorMessage = ( props ) => {
+	if ( ! props.msg ) {
+		return null;
+	}
+
+	const errorMatch = props.msg.message.match( /"message":"(.*?)"}/ );
+	let errorBody = <div>
+		<p><code style="font-size: 12px; word-wrap: break-word">{ props.msg.message }</code></p>
+		<p><a href="https://support.wordpress.com" target="_blank">Please report this error</a></p>
+	</div>
+
+	// Bit of sniffing
+	if ( props.msg.message.indexOf( 'API calls to this blog have been disabled' ) !== -1 ) {
+		errorBody = <p>Jetpack JSON API has been disabled - please <a href="https://apps.wordpress.com/google-docs/support/#json-api" target="_blank">re-enable it</a> to continue posting.</p>
+	} else if ( props.msg.message.indexOf( 'cURL error 28:' ) !== -1 ) {
+		errorBody = <p>Connection to your site timed out. Try again and/or investigate server load.</p>
+	} else if ( props.msg.message.indexOf( '[-32700]' ) !== -1 ) {
+		errorBody = <p>Your site returned an unexpected response - are you <a href="https://apps.wordpress.com/google-docs/support/#other-plugins">running plugins</a> that may be causing a problem?</p>
+	} else if ( props.msg.message.indexOf( 'HTTP status code was not 200' ) !== -1 ) {
+		errorBody = <p>Your server returned an unexpected response. This could be a misconfiguration, a bad plugin, or something may be broken. <a href="https://apps.wordpress.com/google-docs/support/#unexpected-response">Don't panic yet though!</a></p>
+	} else if ( errorMatch && errorMatch.length > 0 ) {
+		errorBody = <p>{ errorMatch[ 1 ] }</p>
+	}
+
+	return <div class="error" style="margin: 0px 6px; position: relative"><p><svg onClick={ this.props.clearError } style="cursor: pointer; position: absolute; right: 5px; top: 12px" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect x="0" fill="none" width="24" height="24"/><g><path d="M17.705 7.705l-1.41-1.41L12 10.59 7.705 6.295l-1.41 1.41L10.59 12l-4.295 4.295 1.41 1.41L12 13.41l4.295 4.295 1.41-1.41L13.41 12l4.295-4.295z"/></g></svg>
+	<strong>Error { props.errorTitle }</strong></p>
+	{ errorBody }
+	</div>
+}
+
+class App extends React.Component {
+	constructor( props ) {
+		super( props )
+		this.state = {
+			sites: [],
+			error: null
+		};
+	}
+
+	componentDidMount() {
+		loadSites().then( ( sites ) => this.setState( { sites } ) );
+	}
+
+	render() {
+		const onError = ( e ) => this.setState( { error: e } )
+		const clearError = () => this.setState( { error: null} )
+
+		return <div className="container">
+			<div className="header">
+				<h1><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect x="0" fill="none" width="24" height="24"/><g><path d="M21 11v8c0 1.105-.895 2-2 2H5c-1.105 0-2-.895-2-2V5c0-1.105.895-2 2-2h8l-2 2H5v14h14v-6l2-2zM7 17h3l7.5-7.5-3-3L7 14v3zm9.94-12.94L15.5 5.5l3 3 1.44-1.44c.585-.585.585-1.535 0-2.12l-.88-.88c-.585-.585-1.535-.585-2.12 0z"/></g></svg> Draft to WordPress</h1>
+
+				<div className="header__help-text">
+					<p>Pick a site to copy this document to below. It will be saved on your site as a draft.</p>
+				</div>
+			</div>
+
+			<div className="sites-list" id="sites-list">
+				<SiteList sites={ this.state.sites } onError={ onError } />
+			</div>
+
+			<ErrorMessage msg={ this.state.error } clearError={ clearError } />
+
+			<div className="footer">
+				<div className="footer__add-site">
+					<a href="<?= authorizationUrl ?>" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect x="0" fill="none" width="24" height="24"/><g><path d="M12 4c4.41 0 8 3.59 8 8s-3.59 8-8 8-8-3.59-8-8 3.59-8 8-8m0-2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm5 9h-4V7h-2v4H7v2h4v4h2v-4h4v-2z"/></g></svg> Add WordPress Site</a>
+				</div>
+				<div className="footer__help-link">
+					<a title="Help" href="https://apps.wordpress.com/support/#faq-googledocs-1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect x="0" fill="none" width="24" height="24"/><g><path d="M12 4c4.41 0 8 3.59 8 8s-3.59 8-8 8-8-3.59-8-8 3.59-8 8-8m0-2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm4 8c0-2.21-1.79-4-4-4s-4 1.79-4 4h2c0-1.103.897-2 2-2s2 .897 2 2-.897 2-2 2c-.552 0-1 .448-1 1v2h2v-1.14c1.722-.447 3-1.998 3-3.86zm-3 6h-2v2h2v-2z"/></g></svg></a>
+				</div>
+			</div>
+		</div>
+	}
+}
+
+function render() {
 	ReactDOM.render(
-		<SiteList sites={sites} />,
-		document.getElementById( 'sites-list' )
+		<App />,
+		document.getElementById( 'container' )
 	);
 }
 
@@ -81,42 +154,7 @@ function reloadSites() {
 }
 
 render()
-reloadSites()
 
 // reload every half hour to refresh the auth url
 const reload = google.script.run.showSidebar.bind( google.script.run );
 window.setTimeout( reload, 1000 * 60 * 30 );
-
-// /**
-//  * Inserts a div that contains an error message after a given element.
-//  *
-//  * @param msg The error message to display.
-//  * @param element The element after which to display the error.
-//  */
-// function showError( msg, element, errorTitle ) {
-// 	var error = '<strong>Error ' + errorTitle + '</strong></p>';
-// 	var errorMatch = msg.message.match( /"message":"(.*?)"}/ );
-
-// 	// Bit of sniffing
-// 	if ( msg.message.indexOf( 'API calls to this blog have been disabled' ) !== -1 ) {
-// 		error += '<p>Jetpack JSON API has been disabled - please <a href="https://apps.wordpress.com/google-docs/support/#json-api" target="_blank">re-enable it</a> to continue posting.</p>';
-// 	} else if ( msg.message.indexOf( 'cURL error 28:' ) !== -1 ) {
-// 		error += '<p>Connection to your site timed out. Try again and/or investigate server load.</p>';
-// 	} else if ( msg.message.indexOf( '[-32700]' ) !== -1 ) {
-// 		error += '<p>Your site returned an unexpected response - are you <a href="https://apps.wordpress.com/google-docs/support/#other-plugins">running plugins</a> that may be causing a problem?</p>';
-// 	} else if ( msg.message.indexOf( 'HTTP status code was not 200' ) !== -1 ) {
-// 		error += '<p>Your server returned an unexpected response. This could be a misconfiguration, a bad plugin, or something may be broken. <a href="https://apps.wordpress.com/google-docs/support/#unexpected-response">Don\'t panic yet though!</a></p>';
-// 	} else if ( errorMatch && errorMatch.length > 0 ) {
-// 		error += '<p>' + errorMatch[ 1 ] + '</p>';
-// 	} else {
-// 		error += '<p><code style="font-size: 12px; word-wrap: break-word">' + msg.message + '</code></p>';
-// 		error += '<p><a href="https://support.wordpress.com" target="_blank">Please report this error</p>';
-// 	}
-
-// 	console.error( msg );
-// 	var div = $( '<div class="error" style="margin: 0px 6px; position: relative"><p><svg style="cursor: pointer; position: absolute; right: 5px; top: 12px" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect x="0" fill="none" width="24" height="24"/><g><path d="M17.705 7.705l-1.41-1.41L12 10.59 7.705 6.295l-1.41 1.41L10.59 12l-4.295 4.295 1.41 1.41L12 13.41l4.295 4.295 1.41-1.41L13.41 12l4.295-4.295z"/></g></svg>' + error + '</div>' );
-// 	$( div ).find( 'svg' ).click( function() {
-// 		$( this ).closest( 'div.error' ).remove();
-// 	} )
-// 	$( element ).after( div );
-// }
