2289ff2237f0037f03f85b23267e01b1df691cfe
George Hotelling <george.hotelling@automattic.com>
2020-11-20 14:48:44 -0500

    Small fix to taxonomies

--- src/server/index.js
+++ src/server/index.js
@@ -98,7 +98,7 @@ function wpDieTemplate( template, error ) {
 const updateSiteInfo = site => {
 	const postTypes = wpClient.getPostTypes( site ).map( postType => {
 		if ( ! postType.publicly_queryable ) {
-			return postType;
+			return Object.assign( {}, postType, { taxonomies: []} );
 		}
 		const taxonomies = wpClient.getTaxonomiesForPostType( site, postType ).map( t => t.name )
 		return Object.assign( {}, postType, { taxonomies } )
